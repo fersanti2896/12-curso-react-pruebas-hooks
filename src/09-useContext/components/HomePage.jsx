f56a646bafa7b7e1f4ec46b3d775c34a6c501df2
Fernando Nicolás <fersanti2896@gmail.com>
2023-02-14 14:36:42 -0600

    Pruebas en HomePage.jsx

--- src/09-useContext/components/HomePage.jsx
+++ src/09-useContext/components/HomePage.jsx
@@ -9,7 +9,7 @@ export const HomePage = () => {
             <h1>{ user?.name }</h1>
             <hr/>
 
-            <pre>
+            <pre aria-label="pre">
                 { JSON.stringify( user, null, 3 ) }
             </pre>
         </>
